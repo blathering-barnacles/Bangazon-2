2415edbb105abc77e6e542e11cb5098037c60c30
rjlancaster <rjlancaster@gmail.com>
2019-02-07 15:56:04 -0600

    commit before navbar download

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 app_name = "ecomm"
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^login$', views.login_user, name='login'),
     url(r'^logout$', views.user_logout, name='logout'),
     url(r'^register$', views.register, name='register'),
     url(r'^sell$', views.sell_product, name='sell'),
     url(r'^products$', views.list_products, name='list_products'),
+    # path('userSettings', views.userSettings, name='userSettings'),
 ]
