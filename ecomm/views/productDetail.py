fa5ca6a81d508396d4b880bee09aa39acc36d61d
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-02-11 11:38:13 -0600

    Added functionality to calculate current quantity of each product is available

--- ecomm/views/productDetail.py
+++ ecomm/views/productDetail.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import RequestContext
+from django.db import connection
 
-from ecomm.models import Product
+from ecomm.models import Product, ProductOrder
 
-def productDetail(request, product_id):
+
+def productDetail(request, pk):
     '''[Gets the product details from the database and prints to the product detail template]
 
     Arguments:
@@ -22,9 +24,24 @@ def productDetail(request, product_id):
     try:
         product_sql = 'SELECT * FROM ecomm_product WHERE ecomm_product.id=%s;'
 
-        product = Product.objects.raw(product_sql, [product_id])[0]
+        product = Product.objects.raw(product_sql, [pk])[0]
 
-        context = {"product": product}
+        purchased_sql = ''' SELECT ecomm_product.id, COUNT(*) as total
+            FROM ecomm_product
+            JOIN ecomm_productorder
+            ON ecomm_productorder.product_id= ecomm_product.id
+            JOIN ecomm_order
+            ON ecomm_productorder.order_id= ecomm_order.id
+            WHERE ecomm_productorder.product_id =%s AND ecomm_order.paymentType_id IS NOT NULL;'''
+
+        purchased_quantity = Product.objects.raw(purchased_sql, [pk])[0]
+
+        # original_qnty_sql = ''' SELECT ecomm_product.quantity
+        #                         FROM ecomm_product
+        #                         WHERE ecomm_product.id = %s;'''
+
+        inventory = product.quantity - purchased_quantity.total
+        context = {"product": product, "inventory": inventory}
         return render(request, 'ecomm/productDetail.html', context)
 
     except IndexError:
