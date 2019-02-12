060c3d84ca87f7532b2c92b3f773e228e9241d5e
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2019-02-12 15:38:15 -0600

    Add docstring to add_product_order view

--- ecomm/views/productOrder.py
+++ ecomm/views/productOrder.py
@@ -11,9 +11,21 @@ from ..models import Order, ProductOrder, Customer
 
 @login_required
 def add_product_to_order(request, product_id):
+    '''[Checks to see if the current logged in user has an existing open order. If so it adds the current product to the order. If not, it creates a new order and adds the product to it]
+
+    Author: Jessica Barnett
+
+    Arguments:
+        request
+        product_id - from the current product detail url
+
+    Returns:
+        Product added to the product-order join table.
+    '''
 
     buyer_id = request.user.id
 
+    # checkes to see if this user has an open order (one that is not deleted and doesn't have a payment type)
     try:
         open_order = Order.objects.raw(''' SELECT * FROM ecomm_order
         WHERE ecomm_order.buyer_id = %s AND ecomm_order.paymentType_id IS NULL AND ecomm_order.deletedOn IS NULL;''', [buyer_id])[0]
@@ -21,6 +33,7 @@ def add_product_to_order(request, product_id):
         with connection.cursor() as cursor:
             cursor.execute("INSERT into ecomm_productorder VALUES (%s,%s,%s,%s);", [None, None, open_order.id, product_id])
 
+    # If a user doesn't have an order, an IndexError is raised. Now an order will be created and the product will be added to that order.
     except IndexError:
         with connection.cursor() as cursor:
             cursor.execute("INSERT into ecomm_order VALUES (%s,%s,%s,%s);", [None, None, buyer_id, None])
