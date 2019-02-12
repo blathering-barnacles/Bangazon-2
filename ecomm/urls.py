1d2f1d1570e069afb4369344e7e98da2bb96a629
Alfonso Miranda <Gabmin21@hotmail.com>
2019-02-11 18:31:34 -0600

    ticket10 is finished

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('userSettings', views.userSettings, name='userSettings'),
     url(r'^shoppingCart/(?P<user_id>\d+)/$', views.cart_items_list, name='list_cart_items'),
     url(r'^deleteItem/(?P<item_id>\d+)/$', views.deleteOrderItem, name='deleteOrderItem'),
-    url(r'^deleteItem/(?P<item_id>\d+)/$', views.deleteOrderItem, name='deleteOrderItem'),
+    url(r'^deleteOrder/(?P<order_id>\d+)/$', views.deleteOrder, name='deleteOrder'),
 ]
